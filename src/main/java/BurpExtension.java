c9f5ac8d1e27cb608055bcaae674fe81d4806245
Anthony Hanel <ahanel13@gmail.com>
2024-07-24 17:18:29 -0600

    - registered all the classes with Montoya api

--- src/main/java/BurpExtension.java
+++ src/main/java/BurpExtension.java
@@ -1,5 +1,10 @@
 import burp.api.montoya.MontoyaApi;
 import burp.api.montoya.extension.ExtensionUnloadingHandler;
+import model.proxy.CustomProxyRequestHandler;
+import model.proxy.CustomProxyResponseHandler;
+import model.proxy.CustomWebSocketCreationHandler;
+import model.scanner.CustomInsertionPointProvider;
+import model.scanner.CustomScanCheck;
 import view.MenuProvider;
 
 ////////////////////////////////////////
@@ -14,20 +19,28 @@ public class BurpExtension implements burp.api.montoya.BurpExtension{
 //-----------------------------------------------------------------------------
 @Override
 public void initialize(MontoyaApi montoyaApi) {
+  _api.logging().logToOutput("Enabling Extension");
+  
   _api             = montoyaApi;
   _menuContext     = new MenuProvider();
   
-  _api.extension().setName("Boiler Plate");
-  _api.logging().logToOutput("Extension Installed");
+  
   _registerUnloader();
+  _setExtensionName();
+  _registerProxyClasses();
+  _registerScanChecks();
+  _registerInsertionPntProvider();
+  
+  _api.logging().logToOutput("Extension Enabled");
 }
 
 ////////////////////////////////////////
 // PRIVATE FIELDS
 ////////////////////////////////////////
+private static final String _EXT_NAME = "Boiler Plate";
+
 private MontoyaApi   _api;
 private MenuProvider _menuContext;
-
 
 ////////////////////////////////////////
 // PRIVATE METHODS
@@ -40,6 +53,36 @@ private void _registerUnloader(){
       _api.logging().logToOutput("Extension unloaded.");
     }
   });
+}
+
+//-----------------------------------------------------------------------------
+private void _setExtensionName(){
+  _api.extension().setName("Boiler Plate");
+}
+
+//-----------------------------------------------------------------------------
+private void _registerProxyClasses(){
+  CustomProxyRequestHandler      reqHandler           = new CustomProxyRequestHandler();
+  CustomProxyResponseHandler     respHandler          = new CustomProxyResponseHandler();
+  CustomWebSocketCreationHandler wsockCreationHandler = new CustomWebSocketCreationHandler();
+  
+  _api.proxy().registerRequestHandler(reqHandler);
+  _api.proxy().registerResponseHandler(respHandler);
+  _api.proxy().registerWebSocketCreationHandler(wsockCreationHandler);
+}
+
+//-----------------------------------------------------------------------------
+private void _registerScanChecks(){
+  CustomScanCheck scanChk = new CustomScanCheck();
+  
+  _api.scanner().registerScanCheck(scanChk);
+}
+
+//-----------------------------------------------------------------------------
+private void _registerInsertionPntProvider(){
+  CustomInsertionPointProvider provider = new CustomInsertionPointProvider();
+  
+  _api.scanner().registerInsertionPointProvider(provider);
 }
 
 
