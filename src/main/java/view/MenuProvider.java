77e9757bcd2a10b320720f2a838d441fa6644831
Anthony Hanel <ahanel13@gmail.com>
2024-11-21 16:16:44 -0700

    Added JavaDocs and notes to MenuProvider

--- src/main/java/view/MenuProvider.java
+++ src/main/java/view/MenuProvider.java
@@ -15,6 +15,30 @@ import java.util.Optional;
 ////////////////////////////////////////
 // CLASS FerretMenuProvider
 ////////////////////////////////////////
+/**
+ * <p>The <code>MenuProvider</code> class is responsible for creating and managing custom context menu items
+ * within the Burp Suite user interface. This class implements the <code>ContextMenuItemsProvider</code> interface
+ * to provide custom menu items based on the context of the selected HTTP request or response.</p>
+ *
+ * <p>When a user right-clicks on an HTTP request or response in Burp Suite, the <code>provideMenuItems</code> method
+ * is called to determine which menu items should be displayed. This class also allows adding action listeners
+ * to the menu items to define custom behavior when the menu items are clicked.</p>
+ *
+ * <p>Key responsibilities of this class include:</p>
+ * <ul>
+ *   <li>Creating menu items "Option 1" and "Option 2"</li>
+ *   <li>Providing menu items based on the context of the selected HTTP request or response</li>
+ *   <li>Allowing action listeners to be added to the menu items</li>
+ *   <li>Storing the context of the selected HTTP request or response for further processing</li>
+ * </ul>
+ *
+ * <p>This class provides a way to extend Burp Suite's context menu functionality by adding custom menu items
+ * and defining their behavior.</p>
+ *
+ * @see burp.api.montoya.ui.contextmenu.ContextMenuItemsProvider
+ * @see burp.api.montoya.ui.contextmenu.ContextMenuEvent
+ * @see burp.api.montoya.ui.contextmenu.MessageEditorHttpRequestResponse
+ */
 public class MenuProvider implements ContextMenuItemsProvider {
 
 
@@ -22,12 +46,22 @@ public class MenuProvider implements ContextMenuItemsProvider {
 // PUBLIC METHODS
 ////////////////////////////////////////
 //-----------------------------------------------------------------------------
+/**
+ * Constructs a new <code>MenuProvider</code> instance and initializes the menu items.
+ */
 public MenuProvider() {
   _option1 = new JMenuItem("Option 1");
   _option2 = new JMenuItem("Option 2");
 }
 
 //-----------------------------------------------------------------------------
+/**
+ * Provides custom menu items based on the context of the selected HTTP request or response.
+ * This method is called by Burp Suite when the user right-clicks on an HTTP request or response.
+ *
+ * @param event the context menu event containing information about the selected HTTP request or response
+ * @return a list of components representing the custom menu items to be displayed
+ */
 @Override
 public List<Component> provideMenuItems(ContextMenuEvent event) {
   Optional<MessageEditorHttpRequestResponse> optional     = event.messageEditorRequestResponse();
@@ -35,14 +69,17 @@ public List<Component> provideMenuItems(ContextMenuEvent event) {
   
   // Todo: Add your own custom logic.
   //  This code determines when a menu item is added.
-  if(
+  
+  if(// Check if the optional contains a value and if the selection context is a request
     optional.isPresent() &&
     optional.get().selectionContext().equals(MessageEditorHttpRequestResponse.SelectionContext.REQUEST)
   ) {
+    // If the selected context is a request, add "Option 1" to the menu
     menuItemList.add(_option1);
+    // Store the selected HTTP request/response for further processing
     _reqResp = optional.get().requestResponse();
   }
-  else {
+  else { // If the selected context is not a request, store the first selected HTTP request/response
     _reqResp = event.selectedRequestResponses().get(0);
   }
   
